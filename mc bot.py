c802608218899f9d32da7717f890f4b6c84655dd
latiao-1337 <146958060+latiao-1337@users.noreply.github.com>
2026-01-26 12:28:22 +0800

    Refactor MCClient to support proxy connections

--- mc bot.py
+++ mc bot.py
@@ -1,112 +1,108 @@
-# 导入socket, threading, random和string模块
 import socket
 import threading
 import random
 import string
+import time
+import socks
 
 
-def generate_random_username():
-    min_length = 4
-    max_length = 8
-    length = random.randint(min_length, max_length)
-    charset = string.ascii_letters + string.digits
-    username = "".join(random.choices(charset, k=length))
-    return username
+def random_username():
+    chars = string.ascii_letters + string.digits
+    return ''.join(random.choices(chars, k=random.randint(4, 8)))
 
 
 class MCClient:
-    try:
-        def __init__(self, host, port):
-            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.socket.connect((host, port))
-            self.socket.setblocking(False)
-            self.recv_thread = threading.Thread(target=self.recv_data)
-            self.recv_thread.start()
-            self.send_handshake_and_login()
-    except:
-        pass
-    def recv_data(self):
-        while True:
+    def __init__(self, host, port, proxy=None):
+        self.host = host
+        self.port = port
+        self.proxy = proxy
+        self.running = True
+        threading.Thread(target=self.loop, daemon=True).start()
+
+    def loop(self):
+        while self.running:
+            sock = None
             try:
-                data = self.socket.recv(1024)
-                if not data:
-                    break
-                # print(f"Received: {data}")
+                if self.proxy:
+                    ip, p = self.proxy.split(':')
+                    sock = socks.socksocket()
+                    sock.set_proxy(socks.SOCKS5, ip, int(p))
+                else:
+                    sock = socket.socket()
+
+                sock.connect((self.host, self.port))
+                sock.setblocking(False)
+
+                self.send_handshake_login(sock)
+                self.recv_loop(sock)
+
             except:
                 pass
+            finally:
+                if sock:
+                    try:
+                        sock.close()
+                    except:
+                        pass
 
-    def send_data(self, data):
-        try:
-            self.socket.send(data)
-            # print(f"Sent: {data}")
-        except:
-            pass
+    def recv_loop(self, sock):
+        while self.running:
+            try:
+                if not sock.recv(1024):
+                    return
+            except BlockingIOError:
+                continue
+            except:
+                return
 
-    def send_handshake_and_login(self):
-        def encode_varint(value):
-            result = b""
-            while True:
-                byte = value & 0x7F
-                value >>= 7
-                if value != 0:
-                    byte |= 0x80
-                result += bytes([byte])
-                if value == 0:
-                    break
-            return result
+    def send_handshake_login(self, sock):
+        def varint(n):
+            b = bytearray()
+            while n:
+                byte = n & 0x7F
+                n >>= 7
+                b.append(byte | (0x80 if n else 0))
+            if not b:
+                b.append(0)
+            return bytes(b)
 
-        def encode_string(value):
-            data = value.encode("utf-8")
-            length = len(data)
-            length = encode_varint(length)
-            return length + data
+        def string(s):
+            data = s.encode()
+            return varint(len(data)) + data
 
-        def encode_handshake_packet():
-            packet_id = 0
-            protocol_version = version
-            server_address = serverip
-            server_port = serverport
-            next_state = 2
-            packet_id = encode_varint(packet_id)
-            protocol_version = encode_varint(protocol_version)
-            server_address = encode_string(server_address)
-            server_port = server_port.to_bytes(2, "big")
-            next_state = encode_varint(next_state)
-            packet_data = (
-                packet_id + protocol_version + server_address + server_port + next_state
-            )
+        # handshake
+        hs = (
+            varint(0) +
+            varint(version) +
+            string(serverip) +
+            serverport.to_bytes(2, 'big') +
+            varint(2)
+        )
+        sock.send(varint(len(hs)) + hs)
 
-            packet_length = len(packet_data)
-            packet_length = encode_varint(packet_length)
-            return packet_length + packet_data
-
-        def encode_login_packet():
-            packet_id = 0
-            username = generate_random_username()
-            packet_id = encode_varint(packet_id)
-            username = encode_string(username)
-            packet_data = packet_id + username
-            packet_length = len(packet_data)
-            packet_length = encode_varint(packet_length)
-            return packet_length + packet_data
-
-        handshake_packet = encode_handshake_packet()
-        login_packet = encode_login_packet()
-
-        self.send_data(handshake_packet)
-        self.send_data(login_packet)
+        # login
+        login = varint(0) + string(random_username())
+        sock.send(varint(len(login)) + login)
 
 
-def create_multiple_clients(host, port):
-    clients = []
-
-    while True:
-        client = MCClient(host, port)
-
-        clients.append(client)
+def run_clients(host, port, proxies):
+    return [MCClient(host, port, p) for p in proxies]
 
 
-serverip = input("serverip(127.0.0.1):")
-serverport = int(input("serverport 端口号(25565):"))
-version = int(input("Protocol 协议版本(https://wiki.vg/Protocol_version_numbers)1.8=47:"))
-clients = create_multiple_clients(serverip, serverport)
+if __name__ == "__main__":
+    serverip = input("server ip: ").strip()
+    serverport = int(input("port: "))
+    version = int(input("protocol version 1.8.9=47: "))
+
+    try:
+        with open("socks5.txt") as f:
+            proxies = [line.strip() for line in f if line.strip()]
+    except FileNotFoundError:
+        proxies = [None]
+
+    clients = run_clients(serverip, serverport, proxies)
+
+
+
+while True:
+        time.sleep(60)
